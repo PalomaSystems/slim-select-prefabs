b3b01aa4b6e12f8efae06eee12520a9acc41dc69
Kyle Scudder <kyle.s@paloma.co.uk>
2023-04-21 11:35:47 +0100

    Added small demo to project

---
+++ demo/js/index.js
@@ -0,0 +1,8 @@
+import { ddlGenericInitialise } from '../../dist/index'
+
+window.addEventListener('load', function () {
+	ddlGenericInitialise()
+	//new SlimSelect({
+	//	select: '#ddlTest'
+	//})
+})
